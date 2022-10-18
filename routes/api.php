649531b4be066098f66e6dfb2187b6c95181807a
Mutia Meilani <87632337+seaaralee@users.noreply.github.com>
2022-10-18 15:31:21 +0700

    migration

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\BookController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,3 +22,16 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::get('/me', [AuthController::class, 'me']);
+
+
+// -- Menggunakan Controller MANUAL
+// Route::get('/books', [BookController::class, 'index']);
+// Route::get('/books\{id}', [BookController::class, 'show']);
+// Route::post('/books', [BookController::class, 'store']);
+// Route::put('/books\{id}', [BookController::class, 'update']);
+// Route::delete('/books\{id}', [BookController::class, 'destroy']);
+
+// -- Menggunakan Controller RESOURCE
+Route::resource('/books', BookController::class)->except(
+    ['create', 'edit']
+);
